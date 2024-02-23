6223f99d8fbbbe595b85ecd52f03cb6a80e49e8e
YHR <1035844140@qq.com>
2024-02-23 09:55:24 +0800

    Update fsd_vec.py

--- ngram_model/fsd_vec.py
+++ ngram_model/fsd_vec.py
@@ -3,58 +3,46 @@ from torch.nn.functional import normalize
 from collections import Counter
 from torch_scatter import scatter_max, scatter_mean
 
-class HiddenSoftNGram(torch.nn.Module):
-    def __init__(self, n, device, tokenizer, sw_coeff=0., stop_words_ids=[], choose="max"):
-        #func inter weighted_inter
+class HiddenSoftNGram:
+    def __init__(self, n, device, vocab_size, sw_coeff=0., stop_words_ids=[], choose="max"):
         super().__init__()
         assert sw_coeff >= 0.
         self.n = n
-        self.n_vocab = len(tokenizer)
+        self.vocab_size = vocab_size
         self.device = device
-        self.previous_hidden_states =None
+        self.hidden_states = None
         self.choose = choose
         self.sw_coeff = sw_coeff
-        self.sw_weight = torch.ones(self.n_vocab, device=self.device, dtype=torch.float)
+        self.sw_weight = torch.ones(self.vocab_size, device=self.device, dtype=torch.float)
         for sid in stop_words_ids:
             self.sw_weight[sid] = self.sw_coeff
 
-    def forward(self,prefix, hidden_states):
+    def penalize(self, prefix, dtype):
         bsz = prefix.shape[0]
-        penalty = torch.zeros((bsz, self.n_vocab), device=self.device, dtype=torch.float)
+        penalty = torch.zeros((bsz, self.vocab_size), device=self.device, dtype=dtype)
         if prefix.shape[1] < self.n:
             return penalty
 
-        if self.previous_hidden_states == None:
-            self.previous_hidden_states = hidden_states[-1][:,:-1]
-            last_hidden_states = hidden_states[-1][:, -1].unsqueeze(1)
-            new_last_hidden_states = hidden_states[-1][:,-self.n+1:].view(bsz,1,-1)
-        else:
-            last_hidden_states = hidden_states[-1]
-            if self.n != 2:
-                new_last_hidden_states = torch.cat((self.previous_hidden_states[:,-self.n+2:],hidden_states[-1]),dim=1).view(bsz,1,-1)
-            else:
-                new_last_hidden_states=hidden_states[-1]
+        query = self.hidden_states[:,-self.n+1:].view(bsz,1,-1)
 
+        seqlen = self.hidden_states.shape[1]
+        keys = [self.hidden_states[:,i:seqlen-self.n+1+i] for i in range(self.n-1)]
+        keys = torch.cat(keys, dim=-1)
 
-        lll = self.previous_hidden_states.shape[1]
-        keys_hidden_states = self.previous_hidden_states[:,:lll-self.n+2]
-        for ttt in range(self.n-2):
-            keys_hidden_states =torch.cat((keys_hidden_states, self.previous_hidden_states[:,1+ttt:lll-self.n+2+ttt+1]), dim=-1)
-
-        temp_penalty = torch.matmul(normalize(new_last_hidden_states,dim=-1),torch.transpose(normalize(keys_hidden_states,dim=-1),1,2))
+        temp_penalty = torch.matmul(normalize(query,dim=-1),torch.transpose(normalize(keys,dim=-1),1,2))
 
 
         if self.choose == "avg":
-            chengfa, argmax = scatter_mean(temp_penalty.squeeze(1), prefix[:,self.n-1:],out=penalty)
+            scatter_mean(temp_penalty.squeeze(1), prefix[:,self.n-1:], out=penalty)
         if self.choose == "max":
-            chengfa, argmax = scatter_max(temp_penalty.squeeze(1), prefix[:,self.n-1:],out=penalty)
+            scatter_max(temp_penalty.squeeze(1), prefix[:,self.n-1:], out=penalty)
 
         penalty = torch.clamp(penalty, min=0, max=1)
-        self.previous_hidden_states = torch.cat((self.previous_hidden_states,last_hidden_states),dim=1)
 
-        return penalty*self.sw_weight
-
-
-
-
-
+        return penalty * self.sw_weight.to(penalty.dtype)
+    
+    def update(self, hidden_states):
+        if self.hidden_states is None:
+            self.hidden_states = hidden_states
+        else:
+            self.hidden_states = torch.cat((self.hidden_states, hidden_states),dim=1)
